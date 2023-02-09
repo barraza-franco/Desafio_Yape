35beb5268b9912f64fe3f264536002bab686057d
Franco Barraza <barrazafranco15@gmail.com>
2023-02-09 00:34:49 -0300

    ejemplo api test
    Se agrega la dependencia axios para realizar steps de apis

---
+++ features/api.feature
@@ -0,0 +1,5 @@
+Feature: The PetStore
+
+  Scenario: As a user, I can create a pet
+
+    Given I realize a get of pet
--- features/step-definitions/steps.ts
+++ features/step-definitions/steps.ts
@@ -2,6 +2,7 @@ import { Given, When, Then } from '@wdio/cucumber-framework';
 
 import LoginPage from '../pageobjects/login.page.js';
 import SecurePage from '../pageobjects/secure.page.js';
+import axios from 'axios';
 
 const pages = {
     login: LoginPage
@@ -18,5 +19,11 @@ When(/^I login with (\w+) and (.+)$/, async (username, password) => {
 Then(/^I should see a flash message saying (.*)$/, async (message) => {
     await expect(SecurePage.flashAlert).toBeExisting();
     await expect(SecurePage.flashAlert).toHaveTextContaining(message);
+});
+
+Given(/^I realize a get of pet$/, async () => {
+    await axios.get("https://petstore.swagger.io/v2/pet/findByStatus?status=available")
+    .then(function (response) {
+        console.log(response);})
 });
 
