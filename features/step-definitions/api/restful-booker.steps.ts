1867671b592da8c82c481e15660b747801ab2d80
Franco Barraza <barrazafranco15@gmail.com>
2023-02-11 03:26:48 -0300

    Update restful-booker.steps.ts

--- features/step-definitions/api/restful-booker.steps.ts
+++ features/step-definitions/api/restful-booker.steps.ts
@@ -4,20 +4,27 @@ import axios from "axios";
 let token;
 let baseUrl;
 let response;
-var headers = {
-    'Content-Type': 'application/json'
+let requestBody;
+let bookingId;
+let headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
 }
 
 Given(/^I set base url '(.+)'$/, async (url) => {
     baseUrl = url;
 });
 
-Given(/^I set path '(.+)'$/, async (path) => {
-    baseUrl += path;
+Given(/^I set path '(.+)'$/, async (path: string) => {
+    baseUrl = baseUrl + "/" + path;
+});
+
+Given(/^I set id stored$/, async () => {
+    baseUrl = baseUrl + "/" + bookingId;
 });
 
 When(/^I do GET$/, async () => {
-    response = await axios.get(baseUrl, { headers: headers }).then(function (response) {
+    response = await axios.get(baseUrl, { headers: headers}).then(function (response) {
         return response;
     }).catch(function (response) {
         return response.response;
@@ -25,7 +32,35 @@ When(/^I do GET$/, async () => {
 });
 
 When(/^I do Post$/, async (body) => {
-    response = await axios.post(baseUrl, JSON.parse(body), { headers: headers }).then(function (response) {
+    requestBody = JSON.parse(body)
+    response = await axios.post(baseUrl, requestBody, { headers: headers }).then(function (response) {
+        return response;
+    }).catch(function (response) {
+        return response.response;
+    });
+});
+
+When(/^I do Put$/, async (body) => {
+    requestBody = JSON.parse(body)
+
+    response = await axios.put(baseUrl, 
+                               requestBody, 
+                             { headers: {
+                                'Content-Type': 'application/json',
+                                'Accept': 'application/json',
+                                'Cookie': 'token='+token
+    }}).then(function (response) {
+        return response;
+    }).catch(function (response) {
+        return response.response;
+    });
+});
+
+When(/^I do DELETE$/, async () => {
+    response = await axios.delete(baseUrl, { headers: {
+                                'Content-Type': 'application/json',
+                                'Cookie': 'token='+token
+    }}).then(function (response) {
         return response;
     }).catch(function (response) {
         return response.response;
@@ -35,10 +70,52 @@ When(/^I do Post$/, async (body) => {
 When(/^I save token$/, async () => {
     token = response.data.token;
     console.log("The token is: "+ token)
+    expect(await token!='').toBe(true)
+});
+
+When(/^I save the first booking id$/, async () => {
+    bookingId = response.data[0].bookingid;
+    console.log("The first booking id is: "+ bookingId)
+    expect(await bookingId!='').toBe(true)
 });
 
 Then(/^I validate status (.+)$/, async (status) => {
     console.log("The response body is: ")
     console.log(response.data)
+    // expect(await response.data!='').toBe(true)
     expect(await response.status.toString()).toBe(status)
+});
+
+Then(/^I validate the body has keys$/, async (body) => {
+    let jsonbody = JSON.parse(body)
+
+    for(let key in jsonbody){
+        expect(await response.data.hasOwnProperty(key)).toBe(true)
+    }
+});
+
+Then(/^I validate the (.+) response body$/, async (method: string) => {
+    if(method=='Post'){
+        expect(await response.data.booking).toStrictEqual(requestBody)
+    }else if(method=='Put'){
+        expect(await response.data).toStrictEqual(requestBody)
+    }
+    
+});
+
+Then(/^I save the id booking created$/, async () => {
+    bookingId = response.data.bookingid;
+    console.log("The id created is: "+ bookingId)
+    expect(await bookingId!='').toBe(true)
+});
+
+Then(/^The booking was delete$/, async () => {
+    response = await axios.get(baseUrl, { headers: headers}).then(function (response) {
+        return response;
+    }).catch(function (response) {
+        return response.response;
+    });
+
+    expect(await response.data).toBe('Not Found')
+    expect(await response.status).toBe(404)
 });
