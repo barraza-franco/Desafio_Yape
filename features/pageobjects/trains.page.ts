cd057f9234481095cb4ebd4d816c1b7698799eef
Franco Barraza <barrazafranco15@gmail.com>
2023-02-14 00:53:59 -0300

    Update trains.page.ts

--- features/pageobjects/trains.page.ts
+++ features/pageobjects/trains.page.ts
@@ -9,18 +9,45 @@ class TrainsPage {
     /**
      * define selectors using getter methods
      */
+    public get peruRailLogo () {
+        return $("//div[@id='logo-perurail']");
+    }
+
     public get continueButton () {
-        return $("//select[@id='Filtros_Ida_Origen']/optgroup");
+        return $("#btnSiguiente_trenes");
+    }
+
+    public get outBoundJourneyFirstOption(){
+        return $("(//table[@id='Table_servicio_ida']/*/tr)[3]/td[5]/span");
+    }
+    
+    public get inBoundJourneyFirstOption(){
+        return $("(//table[@id='Table_servicio_retorno']/*/tr)[3]");
+    }
+
+    public get totalPrice(){
+        return $("//span[@id='liPrecioTotalTrenes']")
     }
 
     public async validatePage(): Promise<boolean>{
-        return (await this.continueButton).isClickable();
+        return await this.peruRailLogo.isDisplayed()
+            && await this.outBoundJourneyFirstOption.isClickable();
+    }
+
+    public async clickOutBoundJourneyFirstOption(){
+        await (await this.outBoundJourneyFirstOption).click();
     }
 
     public async clickContinue(){
+        await (await this.continueButton).waitForClickable();
         await (await this.continueButton).click();
     }
 
+    public async getTotalPrice(): Promise<number>{
+        let totalPrice = await (await this.outBoundJourneyFirstOption.getText()).substring(4);
+ 
+        return Number(totalPrice);
+    }
 }
 
 export default new TrainsPage();
